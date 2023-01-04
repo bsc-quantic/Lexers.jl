b00422856a16d1f85623dd3152c2d2fce612a259
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-04 21:05:35 +0100

    Export new punctuation tokens

--- src/Lexers.jl
+++ src/Lexers.jl
@@ -2,6 +2,6 @@ module Lexers
 
 include("Tokens.jl")
 export Token, pattern, Newline, Whitespace, Keyword, Quote, Number
-export Punctuation, Dot, Comma, Colon, Semicolon, ParenthesisLeft, ParenthesisRight, BracketsLeft, BracketsRight, BracesLeft, BracesRight
+export Punctuation, Dot, Comma, Colon, Semicolon, ParenthesisLeft, ParenthesisRight, BracketsLeft, BracketsRight, BracesLeft, BracesRight, Plus, Minus, GreaterThan, LessThan, Equal, At, Exclamation, Question
 
 end
