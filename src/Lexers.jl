8e31d447aa39dd0a84b0f70745986ad1b7b1336f
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-06 12:01:42 +0100

    Export `Asterisk`, `Slash` symbols

--- src/Lexers.jl
+++ src/Lexers.jl
@@ -2,7 +2,7 @@ module Lexers
 
 include("Tokens.jl")
 export Token, pattern, Newline, Whitespace, Keyword, Quote, Number
-export Punctuation, Dot, Comma, Colon, Semicolon, ParenthesisLeft, ParenthesisRight, BracketsLeft, BracketsRight, BracesLeft, BracesRight, Plus, Minus, GreaterThan, LessThan, Equal, At, Exclamation, Question
+export Punctuation, Dot, Comma, Colon, Semicolon, ParenthesisLeft, ParenthesisRight, BracketsLeft, BracketsRight, BracesLeft, BracesRight, Plus, Minus, Asterisk, Slash, GreaterThan, LessThan, Equal, At, Exclamation, Question
 
 include("Lexer.jl")
 export Lexer, tokenize
