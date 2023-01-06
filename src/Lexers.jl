7b36a082a00be26ee069a4ca5b24efb8d43bea0d
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-07 00:13:38 +0100

    Remove undefined export of `lexeme` symbol
    Replaced with `convert(String, ::Lexeme)`

--- src/Lexers.jl
+++ src/Lexers.jl
@@ -5,7 +5,7 @@ export Token, pattern, Newline, Whitespace, Tab, Keyword, Quote, Natural, Identi
 export Glyph, Dot, Comma, Colon, Semicolon, ParenthesisLeft, ParenthesisRight, BracketLeft, BracketRight, BraceLeft, BraceRight, Plus, Minus, Asterisk, Slash, GreaterThan, LessThan, Equal, At, Exclamation, Question
 
 include("Lexeme.jl")
-export Lexeme, lexeme, token
+export Lexeme, token
 
 include("Lexer.jl")
 export Lexer, tokenize
